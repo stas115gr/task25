1d3b8cf821d31d72e70271f371e59839753dae10
stas115gr <stashauzkin10@gmail.com>
2025-02-17 22:48:50 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12._02
+{
+    class Program
+    {
+        //Выполнить инициализацию одномерного массива с использованием конструктора без указания значений элементов и вывести на экран.
+        static void Main(string[] args)
+        {
+            //task1
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+
+            //}
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+            //Console.Write("n=");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] omas = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"omas[{i}]=");
+            //    omas[i] = int.Parse(Console.ReadLine());
+            //    Console.WriteLine($"omas[{i}]={omas[i]}");
+            //}
+            //Console.ReadKey();
+            //task-2
+            //int[] omas = new[] { -1, 5, 0 - 3, 2, -2 };
+            //foreach (var el in omas)
+            //{
+            //    Console.Write($"{el}\t");
+            //}
+            //Console.ReadKey();
+            //task-3
+            //int[] omas = new int[7];
+            // for (int i = 0; i < omas.Length; i++)
+            // {
+            //     omas[i] = i; 
+            //  }
+
+            // foreach (int el in omas)
+            // {
+            //    Console.Write($"{el}\t");
+            //  }
+            // Console.ReadKey();
+            //task4
+            //int[] omas = new int[6];
+            //Random random = new Random();
+            //for (int i = 0; i < omas.Length; i++)
+            //{
+            //    omas[i] = random.Next(-10, 10);
+
+            //}
+            //foreach (var element in omas)
+            //{
+            //    Console.Write($"{element}\t");
+            //    }
+            //Console.ReadKey();
+        }
+    }
+}
